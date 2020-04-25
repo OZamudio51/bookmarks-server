9a745a627ca628466676ebd9e2312b93d677efd6
Oscar Zamudio <zamudio_1526@hotmail.com>
2020-04-25 11:46:52 -0700

    changes made for assignment

---
+++ src/config.js
@@ -0,0 +1,5 @@
+module.exports ={
+    PORT: process.env.PORT || 8000,
+    NODE_ENV: process.env.PORT || 'development',
+    API_TOKEN: process.env.API_TOKEN || 'adf6d322-ee72-45d2-9d52-2d03cb5fdc22'
+}
